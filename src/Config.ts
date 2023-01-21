75692553e386982a848bef04e0c5848fbdbc95f8
Maurice <mauricewillis44@gmail.com>
2023-01-21 21:35:37 +0800

    feat: Initial project setup

--- src/Config.ts
+++ src/Config.ts
@@ -1,8 +1,8 @@
 /* Environment Variables */
 
 const Config = {
-  nodeEnv: process.env.NODE_ENV || 'development',
   baseUrl: process.env.REACT_APP_BASE_URL,
+  apiKey: process.env.REACT_APP_API_KEY,
 };
 
 export default Config;
